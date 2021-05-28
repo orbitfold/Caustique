e21b59283466bfe3b47ebaa607b41aeda754f3ad
Vytautas Jancauskas <unaudio@gmail.com>
2021-05-28 15:30:36 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+  
+from setuptools import setup, find_packages
+
+setup(
+    name='Caustique',
+    version='0.0.1',
+    url='https://github.com/orbitfold/Caustique.git',
+    author='Vytautas Jancauskas',
+    author_email='unaudio@gmail.com',
+    description='Causal Inference Tools',
+    packages=find_packages(),    
+    install_requires=['numpy'],
+)
